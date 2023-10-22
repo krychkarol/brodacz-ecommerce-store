94c3389f0b29e46bad0e6543485eb76dbe40a619
krychkarol <krych.karol.93@gmail.com>
2023-10-22 16:19:33 +0200

    guardian fix

--- src/request.js
+++ src/request.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = 'https://brodacz-api.herokuapp.com/api/';
+const BASE_URL = 'http://147.135.208.185:5000/api/';
 
 const user = JSON.parse(localStorage.getItem("persist:root"))?.user;
 const currentUser = user && JSON.parse(user).currentUser;
